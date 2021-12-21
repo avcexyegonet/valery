29d660f0ca5e1bdbe1a0e42791237056b88c4ffa
avcexyegonet <grigoriy.solonko@gmail.com>
2021-12-21 15:22:49 +0300

    brightLights

--- webp/src/index.js
+++ webp/src/index.js
@@ -1,28 +1,3 @@
 import './styles/index.scss'
 
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation'
-
-import { diff } from './scripts/_diff.js'
-import { initHeader } from './scripts/_header.js'
-import { initMapInst } from './scripts/_map.js'
-import SwiperSlider from './scripts/_swiper.js'
-
-
-diff()
-initHeader()
-initMapInst()
-
-document.addEventListener('DOMContentLoaded', () => {
-    let sliderArr = document.querySelectorAll('.js-slider')
-
-    sliderArr.forEach((slider, index) => {
-        let options = {
-            root: slider,
-            index: index 
-        }
-        let sliderInst = new SwiperSlider(options)
-        sliderInst.init()
-    })
-})
+console.log('start')
